969e90df88097c2792e2522c1a59a767ce36c535
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-05-13 17:31:00 +0800

    leancloud js

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,32 +2,24 @@
 //获取应用实例
 const app = getApp()
 
-const mock = require('./mock')
-
-const path = require('../../commons/apiPaths')
 
 Page({
   data: {
     items: []
   },
-  showRestaurant(e) {
-    const data = e.currentTarget.dataset;
-    const restaurant = data.restaurant;
-
-    wx.navigateTo({
-      url: `/pages/show/show?id=${restaurant.id}`
-    });
-  },
   onLoad: function () {
-    this.fetchItems(path.getAllRestaurants)
+    
   },
-  fetchItems: function(url) {
-    let that = this
-
-    wx.request({
-      url: url,
-      success: function(res) {
-        that.setData({restaurants:res.data.restaurants})
+  takePhoto: function() {
+    console.log("take photo")
+    wx.chooseImage({
+      count: 1, 
+      sizeType: ['original'], 
+      sourceType: ['album', 'camera'], 
+      success: function (res) {
+       
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
       }
     })
   }
