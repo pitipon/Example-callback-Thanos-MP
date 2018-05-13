325008e001870223350a7a76c1f2cfe1f774fe5c
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-05-13 20:52:48 +0800

    leancloud working

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const AV = require('../../utils/av-weapp-min.js');
 
 Page({
   data: {
@@ -13,14 +13,19 @@ Page({
   takePhoto: function() {
     
     wx.chooseImage({
-      count: 1, 
-      sizeType: ['original'], 
-      sourceType: ['album', 'camera'], 
-      success: function (res) {
-       
-        var tempFilePaths = res.tempFilePaths
-        console.log(tempFilePaths)
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        let tempFilePath = res.tempFilePaths[0];
+        new AV.File('file-name', {
+          blob: {
+            uri: tempFilePath,
+          },
+        }).save().then(
+          file => console.log(file.url())
+        ).catch(console.error);
       }
-    })
+    });
   }
 })
